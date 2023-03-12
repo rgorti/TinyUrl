d2b18a955fa196e7e2cdc81b5dd9331a13a0a1a8
rgorti <30855085+rgorti@users.noreply.github.com>
2023-03-12 15:37:03 +0530

    setupdb.sql added
    Run this command to setup initial mysql db and table

---
+++ src/main/resources/setupdb.sql
@@ -0,0 +1,20 @@
+CREATE DATABASE IF NOT EXISTS tinyurldb;
+CREATE DATABASE classicmodels;
+SHOW DATABASES;
+USE tinyurldb;
+
+
+CREATE TABLE uri_store (
+ id  BIGINT,  
+ short_url  VARCHAR(7)  NOT NULL,
+ source_url  VARCHAR(2048)  NOT NULL,
+ custom_url VARCHAR(50) ,
+ user_id   VARCHAR(50),
+ creation_date Date,
+ expiration_date Date,
+ count mediumint(9) unsigned,
+ PRIMARY KEY (id) 
+	-- INDEX (shorturl)
+    -- INDEX (longurl)
+    -- INDEX (customurl, shorturl)
+ );
